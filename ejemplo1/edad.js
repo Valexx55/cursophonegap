e0880320ef84d0b05bbffdbc9838e1f2a4dc3f19
Val <valexx55@gmail.com>
2021-06-09 19:22:47 +0200

    ejemplo1 edad

--- ejemplo1/edad.js
+++ ejemplo1/edad.js
@@ -1,1 +1,27 @@
-window.alert("EJEMPLO INICIAL");//mostramos ventana emergente comentario
+//window.alert("EJEMPLO INICIAL");//mostramos ventana emergente comentario
+
+function comprobarMayoria() {//inicio
+
+    // window.alert("HA TOCAO EL BOTÓN");
+    console.log("HA TOCAO EL BOTÓN");//
+    
+    //1. OBTENER LA EDAD INTRODUCIDA POR EL USUARIO
+    //declaro una variable que represente la etiqueta edad
+    //var / let
+    var elementoinputedad;
+    elementoinputedad = document.getElementById("edad");//obtengo la caja
+    //console.log(elementoinputedad.id);
+    //console.log(elementoinputedad.value);
+    var edad = elementoinputedad.value;
+    console.log("La edad introducidad es " + edad);
+    //2. COMPROBAR SI ES MAYOR DE 18, MOSTRAR EL MENSAJE  ES MAYOR DE EDAD
+    //3. COMPROBAR SI ES MENOS DE 18, MOSTRAR EL MENSAJE  ES MENOR DE EDAD
+    if (edad >= 18) {
+        window.alert ("ERES MAYOR DE EDAD, PASA");
+    } else {
+        window.alert ("ERES MENOR DE EDAD, NO PASAS");
+    }
+  
+
+
+}//fin
