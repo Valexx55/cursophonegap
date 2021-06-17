cee7ae9296f7b7d3bb02b19c30c707db9c767ca8
valexx55 <valexx55@gmail.com>
2021-06-17 19:21:54 +0200

    ejemplo juego adivina record

--- ejemplo3/adivinanumero.js
+++ ejemplo3/adivinanumero.js
@@ -15,6 +15,7 @@ function compruebaNumero() {
 
     if (numeroIntentos < TOTAL_INTENTOS) {      
         if (incognita == inputNumero) {
+            guardarRecord ();
             document.getElementById("resultado").innerText = "Enhorabuena, has adivinado el número";
             if (window.confirm("¿Te gustaría probar de nuevo...?")) {
                 seguir();
@@ -36,10 +37,47 @@ function compruebaNumero() {
     }
 }
 
+function actualizarNuevoRecord ()
+{ //DRY dont repeat yourself
+    localStorage.setItem('record', numeroIntentos);
+    document.getElementById("logovictoria").hidden = false;
+}
+
+function guardarRecord (){
+    //SI HA TARDADO MENOS VECES QUE EL RECORD ACTUAL, 
+    //TENEES QUE ACTUALIZAR/ GUARDAR EL NUEVO RECORDO
+    //SI NO, NO HAGO NADA
+
+    //1 obtener el record_actual
+    //leer de la memoria 
+    var record_actual = localStorage.getItem('record');
+    if (record_actual==null)
+    {
+        console.log("No hay record guardado");
+        //guardar los intentos actuales como el nuevo recordo
+        actualizarNuevoRecord();
+    } else {
+        console.log("Sí hay record guardado");
+        //2 comparo el recordo actual, con el número de intentos de esta vez
+        if (numeroIntentos<record_actual)
+        {
+            console.log("El usuario ha mejorado la marca");
+            //VISIBILIZAR EL GIF
+            actualizarNuevoRecord();
+        } else {
+            console.log("El usuario NO ha mejorado la marca");
+        }
+    }
+        //caso especial, que no hay record
+    //2 comparo el recordo actual, con el número de intentos de esta vez
+    //3 si el num intentos en menor que record, guardo numero intentos como el nuevo record
+}
+
 function seguir() {
     numeroIntentos = 1;
     incognita = parseInt((Math.random() * NUMERO) + 1);
     document.getElementById("intentos").innerText = "Número de intentos: " + numeroIntentos;
+    document.getElementById("logovictoria").hidden = true;//ocultar el record
     cargarNumero();
 }
 
