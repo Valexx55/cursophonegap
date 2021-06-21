e967a806ef13b47a04809a0443a25a073080fab5
valexx55 <valexx55@gmail.com>
2021-06-21 20:59:46 +0200

    perros 2 de 2

--- ejemplo6/perros.js
+++ ejemplo6/perros.js
@@ -7,7 +7,7 @@ PASO 3: DIBUJAR LA INFORMACIÓN QUE TRAE LA RESPUESTA
 
 JAVASCRIPT
 
-    - AJAX: Javascritp asíncrono
+    - AJAX: Javascritp asíncrono XMLHttpRequest
     - FETCH: Capturar
 
     Con estas dos variantes lo que hacemos es enviar y recibir 
@@ -17,7 +17,28 @@ JAVASCRIPT
 const API_WEB_PERROS = "https://dog.ceo/api/breeds/image/random";
 var http_request;//ámbito global
 
-function pedirPerro() {
+function actualizarIMG (url_perro)
+{
+    let elemento_img_perro = document.getElementById("imgperro");
+    elemento_img_perro.src = url_perro;
+    elemento_img_perro.hidden = false;
+}
+
+function pedirPerroFetch() {
+    fetch(API_WEB_PERROS) //PROMESAS -
+        .then(response => response.json()) //funciones de flecha //anónimas -response es el cuerpo HTTP de vuelta
+        .then(infoperro => { //inicio función 
+            console.log(infoperro);
+            actualizarIMG(infoperro.message);
+
+        }//final de la función
+
+        ).catch(function(error) {
+            console.log('Hubo un problema con la petición Fetch:' + error.message);
+          });
+}
+
+function pedirPerroAjax() {
     //VAMOS A USAR AJAX PARA OBTENER LA IMAGEN DEL PERRO
     http_request = new XMLHttpRequest(); //ESTO SIRVE PARA ENVIAR Y RECIBIR LA INFO DEL SERVIDOR
     http_request.onreadystatechange = procesarRespuestaServidor;//cuando te responda el servidor, me llamas a esa función
@@ -28,13 +49,30 @@ function pedirPerro() {
 
 function procesarRespuestaServidor() {
 
-    if (http_request.readyState == 4) {
-        if (http_request.status == 200) {
-            alert(http_request.responseText);//ESTO ES EL JSON QUE RECIBO
-            //TODO representarlo
+    if (http_request.readyState == 4) //la comunicación se ha completado
+    {
+
+        if (http_request.status == 200) { //la cosa ha ido bien
+            console.log(http_request.responseText);//ESTO ES EL JSON QUE RECIBO
+            procesarCuerpo(http_request.responseText);
         } else {
             alert('Hubo problemas con la petición.');
         }
     }
 
+}
+
+function procesarCuerpo(cuerpo) {
+    //TODO representarlo 
+    //recibiendo texto json y lo tengo que pasar a variable --> DESERILIZAR
+    //SERIALIZAR // DESERIALIZAR
+    let info_perro = JSON.parse(cuerpo);//paso de texto a variable
+    // {
+    //     "message": "https://images.dog.ceo/breeds/terrier-bedlington/n02093647_529.jpg",
+    //     "status": "success"
+    // }
+    console.log(" RUTA FOTO PERRO " + info_perro.message);
+    console.log(" ESTATUS " + info_perro.status);
+    //obtengo el elmento imagen
+    actualizarIMG(info_perro.message);
 }
