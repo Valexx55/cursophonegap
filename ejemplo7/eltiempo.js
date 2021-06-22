1323f61dc411f675b6d067d03317879b40b94784
valexx55 <valexx55@gmail.com>
2021-06-22 21:02:28 +0200

    tiempo 3 de 3

--- ejemplo7/eltiempo.js
+++ ejemplo7/eltiempo.js
@@ -14,6 +14,7 @@ function pedirTiempoFetch() {
         .then(response => response.json()) //funciones de flecha //anónimas -response es el cuerpo HTTP de vuelta
         .then(infotiempo => { //inicio función 
             console.log(infotiempo);
+            procesarInfoTiempo(infotiempo);
             //actualizarIMG(infotiempo.message);
 
         }//final de la función
@@ -23,8 +24,78 @@ function pedirTiempoFetch() {
           });
 }
 
+/***
+ * 
+ * let unix_timestamp = 1549312452
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+var date = new Date(unix_timestamp * 1000);
+// Hours part from the timestamp
+var hours = date.getHours();
+// Minutes part from the timestamp
+var minutes = "0" + date.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + date.getSeconds();
 
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+console.log(formattedTime);
+
+ */
+
+function traducirMomento (tiempoms)
+{
+    let momentohhmmss;
+
+        var date = new Date(tiempoms * 1000);
+        var hours = date.getHours();
+        var minutes = "0" + date.getMinutes();
+        var seconds = "0" + date.getSeconds();
+        
+        // Will display time in 10:30:23 format
+        momentohhmmss = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    
+    return momentohhmmss;
+}
 
 function procesarInfoTiempo(cuerpo) {
     //ACCEDER A LA INFO DEL JSON 
+    console.log("Longuitud " + cuerpo.coord.lon);
+    console.log("Latitud " + cuerpo.coord.lat);
+    console.log("Descripción " + cuerpo.weather[0].description);//cuerpo['weather'][0].description
+
+    let lista_tds = document.getElementsByTagName("td");
+    console.log(lista_tds.length);
+    //http://openweathermap.org/img/wn/10d@2x.png
+    //mostramos el icono del tiempo
+    lista_tds[0].firstChild.src = "http://openweathermap.org/img/wn/"+cuerpo.weather[0].icon+"@2x.png";
+    //descripcion
+    lista_tds[1].innerHTML = cuerpo.weather[0].description;
+    //temperatura
+    lista_tds[2].innerHTML = cuerpo.main.temp + " Cº";
+    //humedad
+    lista_tds[3].innerHTML = cuerpo.main.humidity + " %";
+    //viento
+    lista_tds[4].innerHTML = cuerpo.wind.speed + " m/s";
+    //amananece
+    lista_tds[5].innerHTML = traducirMomento(cuerpo.sys.sunrise);
+    //anocher
+    lista_tds[6].innerHTML = traducirMomento(cuerpo.sys.sunset);
+
+     //console.log("amanenece " + traducirMomento(cuerpo.sys.sunrise));
+
+    //ATRIBUTOS DE INTERÉS
+    /**
+     * 
+     * nombre weather[0].description
+     * temperatura main.temp 
+     * humedad  humidity
+     * icono weather[0].icon
+     * viento main.speed
+     * salida del sol sys.sunrise
+     * puesta sys.sunset
+     * 
+    */
+
 }
