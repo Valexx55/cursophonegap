96e93c559a5ab905812416ea2d9583b913d00f45
valexx55 <valexx55@gmail.com>
2021-07-07 21:01:20 +0200

    pantall detalle fin

--- EventosMadrid/www/js/detalleevento.js
+++ EventosMadrid/www/js/detalleevento.js
@@ -1,7 +1,8 @@
 this.onload = dibujarPantalla;
 
 let url_evento;
-let detalle_evento;
+let detalle_evento;//info inicial pasada por session storage
+let detalle2_evento;//info obtenida tras nueva petición 
 
 function dibujarPantalla() {
     let evento_json = sessionStorage.getItem('evento');
@@ -36,7 +37,7 @@ function obtenerDatosEvento(url_evento) {
             console.log(info_evento);
             dibujarMapa(detalle_evento.location.latitude, detalle_evento.location.longitude);
             mostrarEvento(info_evento);
-
+            detalle2_evento = info_evento;
             //lc.dismiss();//quitamos el circulito de espera
         }).catch(error => {
             //lc.dismiss();
@@ -54,82 +55,139 @@ function obtenerDatosEvento(url_evento) {
  */
 
 function mostrarEvento(info_evento) {
-    //console.log(info_evento['@graph'][0].title);
     mostrarTitulo(info_evento['@graph'][0].title);
-    //console.log(info_evento['@graph'][0].description);
-
-    //console.log(info_evento['@graph'][0].dtstart);
     mostrarHoraInicio(info_evento['@graph'][0].dtstart);
-    // console.log(info_evento['@graph'][0].address.locality);
-    // console.log(info_evento['@graph'][0].address['postal-code']);
-    // console.log(info_evento['@graph'][0].address['street-address']);
-    mostrarDireccion(info_evento['@graph'][0].address['street-address'] + "<br>" + info_evento['@graph'][0].address['postal-code'] + " " + info_evento['@graph'][0].address.locality)
+    mostrarDireccion(info_evento['@graph'][0]['event-location'] + "<br>" + info_evento['@graph'][0].address['street-address'] + "<br>" + info_evento['@graph'][0].address['postal-code'] + " " + info_evento['@graph'][0].address.locality)
     mostrarDescripcion(info_evento['@graph'][0].description);
-    infocomollegar (detalle_evento.location.latitude, detalle_evento.location.longitude);
+    mostrarEnlaceExterno(info_evento['@graph'][0].link);
+
+    infocomollegar(detalle_evento.location.latitude, detalle_evento.location.longitude);
 }
 
-function infocomollegar (lat, long)
-{
-    document.getElementById("enlacecll").href = "https://www.google.com/maps/search/?api=1&query="+lat+"%2C"+long;
+function infocomollegar(lat, long) {
+    document.getElementById("enlacecll").href = "https://www.google.com/maps/search/?api=1&query=" + lat + "%2C" + long;
 }
 
-function agendar ()
-{
-    var startDate = new Date(2021,6,7,18,30,0,0,0); // beware: month 0 = january, 11 = december
-    var endDate = new Date(2021,6,7,19,30,0,0,0);
-    var title = "My nice event";
-    var eventLocation = "Home";
-    var notes = "Some notes about this event.";
-    var success = function(message) { alert("Success: " + JSON.stringify(message)); };
-    var error = function(message) { alert("Error: " + message); };
-  
-  
-   // window.plugins.calendar.createEventInteractively(title,eventLocation,notes,startDate,endDate,success,error);
-  
-    // create a calendar (iOS only for now)
-    //window.plugins.calendar.createCalendar(calendarName,success,error);
-    // if you want to create a calendar with a specific color, pass in a JS object like this:
-   /* var createCalOptions = window.plugins.calendar.getCreateCalendarOptions();
-    createCalOptions.calendarName = "My Cal Name";
-    createCalOptions.calendarColor = "#FF0000"; // an optional hex color (with the # char), default is null, so the OS picks a color
-    window.plugins.calendar.createCalendar(createCalOptions,success,error);*/
-  
-    // delete a calendar
-   // window.plugins.calendar.deleteCalendar(calendarName,success,error);
-  
-    // create an event silently (on Android < 4 an interactive dialog is shown)
-  
-    let id_evento = window.plugins.calendar.createEvent(title,eventLocation,notes,startDate,endDate,success,error);
-    alert ("ID EVENTO = "+ id_evento);
-    //MÉTODO QUE BUSCA EVENTOS Y DEVUELVE JSON CON LA INFO
-    window.plugins.calendar.findEvent(title,eventLocation,notes,startDate,endDate,success,error);
-  
-    //MÉTODO QUE ABRE EL CALENDARIO EN UNA FECHA CONCRETA
-    var d = new Date(new Date().getTime() + 3*24*60*60*1000);
-    window.plugins.calendar.openCalendar(d, success, error); // callbacks are optional 
+
+function obtenerAnio(fecha) {
+    let anio;
+
+    anio = fecha.slice(0, fecha.length - 17);
+
+    return anio;
+}
+
+function obtenerMes(fecha) {
+    let mes;
+
+    mes = fecha.slice(5, fecha.length - 14);
+    mes--;
+
+    return mes;
+}
+
+function obtenerDia(fecha) {
+    let dia;
+
+    dia = fecha.slice(8, fecha.length - 11);
+
+    return dia;
+
+}
+
+function obtenerHora(fecha) {
+    let hora;
+
+    hora = fecha.slice(11, fecha.length - 8);
+
+    return hora;
+}
+
+function obtenerMinutos(fecha) {
+    let minutos;
+
+    minutos = fecha.slice(14, fecha.length - 5);
+
+    return minutos;
+}
+
+
+async function infoExitoAgenda() {
+    const toast = document.createElement('ion-toast');
+    toast.message = 'EVENTO AGENDADO CORRECTAMENTE EN SU CALENDARIO';
+    toast.duration = 5000;
+    toast.color = "success";
+    toast.position = "middle";
+
+    document.body.appendChild(toast);
+
+    //todo GUARDAR LA INFO DEL EVENTO AGENDADO
+    //serilizar un array en localStorage de detalleevento2
+    //y luego recuperarlo la pantalla de mostrar favoritos o agendados
+
+    return toast.present();
+}
+
+async function infoFalloAgenda() {
+    const toast = document.createElement('ion-toast');
+    toast.message = 'NO PUDO AGENDARSE EL EVENTO EN SU CALENDARIO';
+    toast.duration = 5000;
+    toast.color = "danger";
+    toast.position = "middle";
+
+    document.body.appendChild(toast);
+    return toast.present();
+}
+
+
+
+function agendar() {
+
+    console.log(" AÑO " + obtenerAnio(detalle2_evento['@graph'][0].dtstart));
+    console.log(" MES " + obtenerMes(detalle2_evento['@graph'][0].dtstart));
+    console.log(" DIA " + obtenerDia(detalle2_evento['@graph'][0].dtstart));
+    console.log(" HORA " + obtenerHora(detalle2_evento['@graph'][0].dtstart));
+    console.log(" MINUTOS " + obtenerMinutos(detalle2_evento['@graph'][0].dtstart));
+
+
+    var startDate = new Date(obtenerAnio(detalle2_evento['@graph'][0].dtstart), obtenerMes(detalle2_evento['@graph'][0].dtstart), obtenerDia(detalle2_evento['@graph'][0].dtstart), obtenerHora(detalle2_evento['@graph'][0].dtstart), obtenerMinutos(detalle2_evento['@graph'][0].dtstart), 0, 0, 0); // beware: month 0 = january, 11 = december
+    var endDate = new Date(obtenerAnio(detalle2_evento['@graph'][0].dtend), obtenerMes(detalle2_evento['@graph'][0].dtend), obtenerDia(detalle2_evento['@graph'][0].dtend), obtenerHora(detalle2_evento['@graph'][0].dtend), obtenerMinutos(detalle2_evento['@graph'][0].dtend), 0, 0, 0);;
+    var title = detalle2_evento['@graph'][0].title;
+
+    var eventLocation = detalle2_evento['@graph'][0]['event-location'] + " " + detalle2_evento['@graph'][0].address['street-address'] + " " + detalle2_evento['@graph'][0].address['postal-code'] + " " + detalle2_evento['@graph'][0].address.locality;
+    var notes = detalle2_evento['@graph'][0].description;
+    var success = function (message) { infoExitoAgenda(); };
+    var error = function (message) { infoFalloAgenda(); }
+
+ 
+
+    //EL QUE APUNTA EL CALNDARIO REALMENTE
+
+    window.plugins.calendar.createEvent(title, eventLocation, notes, startDate, endDate, success, error);
+
+
 }
 
 function mostrarDireccion(direccion) {
     let label = document.createElement('ion-label');
-    // let label = document.createElement('ion-textarea');
-    //label.setAttribute("rows", '2'); //<ion-icon name="location-outline"></ion-icon>
+   
     let icono = document.createElement('ion-icon');
-    icono.setAttribute('name','location-outline');
+    icono.setAttribute('name', 'location-outline');
 
     let item = document.createElement('ion-item');
     label.innerHTML = direccion;
     item.appendChild(icono);
     item.appendChild(label);
     document.getElementById("infoevento").appendChild(item);
-    //document.createElement('ion-label');
+   
 }
 
 function mostrarHoraInicio(horainicio) {
     let label = document.createElement('ion-label');
     let item = document.createElement('ion-item');
     let icono = document.createElement('ion-icon');
-    icono.setAttribute('name','calendar-outline');//<ion-icon name="calendar-outline"></ion-icon>
-    label.innerHTML = horainicio.slice (0, horainicio.length-5);
+    icono.setAttribute('name', 'calendar-outline');//<ion-icon name="calendar-outline"></ion-icon>
+    label.innerHTML = horainicio.slice(0, horainicio.length - 5);
     item.appendChild(icono);
     item.appendChild(label);
     document.getElementById("infoevento").appendChild(item);
@@ -141,14 +199,14 @@ function mostrarHoraInicio(horainicio) {
 function mostrarTitulo(titulo) {
     let label = document.createElement('ion-label');
     let item = document.createElement('ion-item');
-    let icono = document.createElement('ion-icon');   //<ion-icon name="accessibility-outline"></ion-icon>
+    let icono = document.createElement('ion-icon');   
     icono.setAttribute("name", 'accessibility-outline');
     label.innerHTML = titulo;
     item.appendChild(icono);
     item.appendChild(label);
 
     document.getElementById("infoevento").appendChild(item);
-    //document.createElement('ion-label');
+   
 }
 
 
@@ -161,13 +219,62 @@ function mostrarDescripcion(descripcion) {
         icono.setAttribute("name", 'information-circle-outline');
         let item = document.createElement('ion-item');
         label.innerHTML = descripcion;
-        
+
         item.appendChild(icono);
         item.appendChild(label);
         document.getElementById("infoevento").appendChild(item);
     }
     //document.createElement('ion-label');
 }
+
+function compartir()
+{
+    // this is the complete list of currently supported params you can pass to the plugin (all optional)
+var options = {
+    message: '¿Te apuntas?', // not supported on some apps (Facebook, Instagram)
+    subject: 'Evento en Madrid', // fi. for email
+    //files: ['', ''], // an array of filenames either locally or remotely
+    //url: 'https://www.website.com/foo/#bar?a=b',
+    url: detalle2_evento['@graph'][0].link,
+    chooserTitle: 'Elije ...' // Android only, you can override the default share sheet title
+    //appPackageName: 'com.apple.social.facebook', // Android only, you can provide id of the App you want to share with
+    //iPadCoordinates: '0,0,0,0' //IOS only iPadCoordinates for where the popover should be point.  Format with x,y,width,height
+  };
+  
+  var onSuccess = function(result) {
+    console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+    console.log("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+  };
+  
+  var onError = function(msg) {
+    console.log("Sharing failed with message: " + msg);
+  };
+  
+  window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+}
+
+function mostrarEnlaceExterno(link) {
+
+
+    let boton = document.createElement('ion-button');
+    boton.setAttribute('href', link);
+    boton.innerHTML = "MÁS INFO";
+    boton.color = 'secondary';
+    let icono = document.createElement('ion-icon');
+    icono.setAttribute("name", 'arrow-redo-outline');
+
+    let item = document.createElement('ion-item');
+
+    item.appendChild(icono);
+    item.appendChild(boton);
+    document.getElementById("infoevento").appendChild(item);
+
+    //document.createElement('ion-label');
+}
+
+//compartir por whatsapp el evento o para compartir más en general??
+
+
 
 
 async function mostrarAvisoFallo() {
